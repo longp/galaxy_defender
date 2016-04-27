971356c2d4fd76089e1c3da1d6c83ac0a86f16bb
longp <lphan1989@gmail.com>
2016-04-26 22:13:12 -0400

    added process env port for heroku deploy and added quadtreee logic

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require('morgan');
 var app = express();
+var PORT = process.env.PORT
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/galaxy.html')
@@ -10,4 +11,4 @@ app.use("/imgs",express.static("public/imgs"))
 app.use("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
 
-app.listen(3535)
+app.listen(PORT)
